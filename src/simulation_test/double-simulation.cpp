27729254b3af85ccad0c8ef1d032637a2a64cf4e
Zoe Wall <40009273@napier.ac.uk>
2018-03-16 16:21:48 +0000

    fixed descriptor sets for double

--- src/simulation_test/double-simulation.cpp
+++ src/simulation_test/double-simulation.cpp
@@ -41,7 +41,7 @@ void double_simulation::createDescriptorPool()
 	poolSize[1].type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
 	poolSize[1].descriptorCount = 2;
 	poolSize[2].type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-	poolSize[2].descriptorCount = 1;
+	poolSize[2].descriptorCount = 2;
 
 
 	VkDescriptorPoolCreateInfo poolInfo = {};
